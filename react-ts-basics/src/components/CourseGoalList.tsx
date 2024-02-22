108135ca211dacc50c2b0c40999f931abe08afa1
Jeff Lintel <jefflintel@gmail.com>
2024-02-22 11:46:10 -0600

    flexible infobox component with discriminated union type

--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,13 +1,33 @@
+import { ReactNode } from "react";
 import CourseGoal from "./CourseGoal.tsx"; //.tsx included because it's best practice for vite projects
 import { type CourseGoal as CGoal } from "../App.tsx";
+import InfoBox from "./InfoBox.tsx";
 
 type CourseGoalListProps = {
   goals: CGoal[];
   onDeleteGoal: (id: number) => void;
 };
 
-export default function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
+export default function CourseGoalList({
+  goals,
+  onDeleteGoal,
+}: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint" >
+        You don't have any course goals yet. Add some!
+      </InfoBox>
+    );
+  }
+  
+let warningBox: ReactNode;
+
+if (goals.length >= 4 ) {
+    warningBox = <InfoBox mode="warning" severity="medium">Wow! That's a lot of goals. Maybe focus on a few so you're sure you succeed!</InfoBox>
+}
   return (
+    <>
+    {warningBox}
     <ul>
       {goals.map((goal) => (
         <li key={goal.id}>
@@ -17,5 +37,5 @@ export default function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListPr
         </li>
       ))}
     </ul>
-  );
-}
+  </>
+)}
